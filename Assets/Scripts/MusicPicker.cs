a06aa93c5f778860e0603449a4bc055d287748fd
Yonatan Koritny <yonatan.koritny@gmail.com>
2025-07-29 19:59:55 +0300

    Random Music

---
+++ Assets/Scripts/MusicPicker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPicker : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private List<AudioClip> musicClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load all audio clips from Resources/Music
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Music");
+        musicClips = new List<AudioClip>(clips);
+
+        if (musicClips.Count > 0)
+        {
+            PlayRandomSong();
+        }
+    }
+
+    void PlayRandomSong()
+    {
+        int index = Random.Range(0, musicClips.Count);
+        audioSource.clip = musicClips[index];
+        audioSource.Play();
+    }
+}
